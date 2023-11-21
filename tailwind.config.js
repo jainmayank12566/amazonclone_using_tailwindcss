a9fca391261dad47620297984b92211123a618a6
jainmayank12566 <jainmayank12566@gmail.com>
2023-11-21 19:27:19 +0530

    firstcommit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.html"],
+  theme: {
+    extend: {
+      colors:{
+        '#ffe5b4':'#ffe5b4'
+      },
+    },
+  },
+  plugins: [],
+}
+
